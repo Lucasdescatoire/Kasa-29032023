dd0f79de182b0a40f974dfcc56fdcdce4af16eeb
Lucasdescatoire <72078978+Lucasdescatoire@users.noreply.github.com>
2023-04-12 10:57:41 +0200

    Resolution prob page+contenue/commentaire

--- src/Components/Tags.js
+++ src/Components/Tags.js
@@ -1,16 +1,15 @@
-import React from "react";
+import React from 'react';
 import "../Styles/Tags.scss"
 
-function Tags (props) {
-
-    /*return (
-        <div>
-            <p>{props.tags}</p>
+function Tag(props) {
+    return (
+        <div className="tag">
+            <p className="tag-content">{props.content}</p>
         </div>
-    ) */
+    )
 
 
 
 }
 
-export default Tags
+export default Tag
