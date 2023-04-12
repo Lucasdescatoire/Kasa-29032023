d220b259741a744954dc1dbbf299c5345cad8ea4
Lucasdescatoire <72078978+Lucasdescatoire@users.noreply.github.com>
2023-04-12 08:53:22 +0200

    Init + image + lorem

---
+++ src/Components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <h1>
+                A propos
+                {/*test du lorem réussi*/}
+                Platea primis gravida tamquam mentitum hendrerit.
+                Interdum aliquid tantas agam efficiantur. Dictum vocent regione eloquentiam molestie ei eloquentiam
+                electram feugait reformidans.
+            </h1>
+        </div>
+    )
+}
+
+export default About
---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logoFooter from"../Assets/logoFooter.svg"
+ 
+
+
+function Footer() {
+    return (
+        <div>
+          <img src={logoFooter} alt="logo_footer"/>
+            <h1>
+            © 2020 Kasa. All rights reserved
+            </h1>
+        </div>
+    )
+
+}
+export default Footer
