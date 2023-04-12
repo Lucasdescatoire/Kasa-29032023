538d2c1391999089fb5eccfd9eab5478fae46fc2
Lucasdescatoire <72078978+Lucasdescatoire@users.noreply.github.com>
2023-04-12 10:05:12 +0200

    Slider+ page de location avec tout les composant

---
+++ src/Components/Home_Components/HomesComponents.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../Styles/Homes_Components.scss"
+
+function HomesComponents (props) {
+  return (
+        <div>
+            <Link className="LocationLinks" to={"/locations/"+props.location.id}>
+            <div className="LocationLinks_Card" style={
+
+{ backgroundImage:`url(${props.location["cover"]})`}
+}>
+<p className="LocationLinks_Title">{props.location.title}</p>
+
+                </div>
+            </Link>
+        </div>
+)
+}
+
+export default HomesComponents
---
+++ src/Components/Tags.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../Styles/Tags.scss"
+
+function Tags (props) {
+
+    /*return (
+        <div>
+            <p>{props.tags}</p>
+        </div>
+    ) */
+
+
+
+}
+
+export default Tags
